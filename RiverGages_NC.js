ddb3a26538518f0fa391739acde0ecffa44cbe65
Anna <cherry5@live.unc.edu>
2020-04-22 19:52:50 -0400

    Final Project

---
+++ RiverGages_NC.js
@@ -0,0 +1,352 @@
+var RiverGages = {
+    "type" : "FeatureCollection",
+    "crs" : {
+      "type" : "name",
+      "properties" : {
+        "name" : "EPSG:4326"
+      }
+    },
+    "features" : [
+      {
+        "type" : "Feature",
+        "id" : 0,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -82.1117,
+            35.4233
+          ]
+        },
+        "properties" : {
+          "FID" : 0,
+          "FID_1" : 1,
+          "Basin" : "BRD",
+          "Sq_Miles" : 1513.8948122300001,
+          "Acres" : 968892.679825,
+          "Name" : "Rutherford",
+          "ORIG_FID" : 0
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 1,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -80.9832,
+            35.5564
+          ]
+        },
+        "properties" : {
+          "FID" : 1,
+          "FID_1" : 2,
+          "Basin" : "Burke",
+          "Sq_Miles" : 3285.4051448800001,
+          "Acres" : 2102659.2927199998,
+          "Name" : "Catawba",
+          "ORIG_FID" : 1
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 2,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -76.9842,
+            36.0478
+          ]
+        },
+        "properties" : {
+          "FID" : 2,
+          "FID_1" : 3,
+          "Basin" : "Bertie",
+          "Sq_Miles" : 1298.2831911200001,
+          "Acres" : 830901.24231899995,
+          "Name" : "Chowan",
+          "ORIG_FID" : 2
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 3,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -78.8236,
+            35.8356
+          ]
+        },
+        "properties" : {
+          "FID" : 3,
+          "FID_1" : 4,
+          "Basin" : "NewHanover",
+          "Sq_Miles" : 9163.5949759699997,
+          "Acres" : 5864700.78462,
+          "Name" : "Cape Fear",
+          "ORIG_FID" : 3
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 4,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -82.5781,
+            35.6089
+          ]
+        },
+        "properties" : {
+          "FID" : 4,
+          "FID_1" : 5,
+          "Basin" : "Buncombe",
+          "Sq_Miles" : 2828.80611649,
+          "Acres" : 1810435.9145599999,
+          "Name" : "French Broad",
+          "ORIG_FID" : 4
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 5,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -84.0028,
+            35.0814
+          ]
+        },
+        "properties" : {
+          "FID" : 5,
+          "FID_1" : 6,
+          "Basin" : "Cherokee",
+          "Sq_Miles" : 644.33604328399997,
+          "Acres" : 412375.06770199997,
+          "Name" : "Hiwassee",
+          "ORIG_FID" : 5
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 6,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -78.5483,
+            34.095
+          ]
+        },
+        "properties" : {
+          "FID" : 6,
+          "FID_1" : 7,
+          "Basin" : "Brunswick",
+          "Sq_Miles" : 3329.3733686999999,
+          "Acres" : 2130798.95597,
+          "Name" : "Lumber",
+          "ORIG_FID" : 6
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 7,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -83.3714,
+            35.1858
+          ]
+        },
+        "properties" : {
+          "FID" : 7,
+          "FID_1" : 8,
+          "Basin" : "Macon",
+          "Sq_Miles" : 1797.11209082,
+          "Acres" : 1150151.73813,
+          "Name" : "Little Tennessee",
+          "ORIG_FID" : 7
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 8,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -77.3028,
+            35.3139
+          ]
+        },
+        "properties" : {
+          "FID" : 8,
+          "FID_1" : 9,
+          "Basin" : "Craven",
+          "Sq_Miles" : 6062.1185777500004,
+          "Acres" : 3879755.8897600002,
+          "Name" : "Neuse",
+          "ORIG_FID" : 8
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 9,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -80.7903,
+            35.4678
+          ]
+        },
+        "properties" : {
+          "FID" : 9,
+          "FID_1" : 10,
+          "Basin" : "Merlenburg",
+          "Sq_Miles" : 0,
+          "Acres" : 0,
+          "Name" : "Catawba",
+          "ORIG_FID" : 9
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 10,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -76.3425,
+            36.4214
+          ]
+        },
+        "properties" : {
+          "FID" : 10,
+          "FID_1" : 11,
+          "Basin" : "Camden",
+          "Sq_Miles" : 3366.4597307600002,
+          "Acres" : 2154534.2276900001,
+          "Name" : "Pasquotank",
+          "ORIG_FID" : 10
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 11,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -80.3031,
+            36.515
+          ]
+        },
+        "properties" : {
+          "FID" : 11,
+          "FID_1" : 12,
+          "Basin" : "Stokes",
+          "Sq_Miles" : 3492.8599835099999,
+          "Acres" : 2235430.3894400001,
+          "Name" : "Roanoke",
+          "ORIG_FID" : 11
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 12,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -79.0956,
+            36.0711
+          ]
+        },
+        "properties" : {
+          "FID" : 12,
+          "FID_1" : 13,
+          "Basin" : "Orange",
+          "Sq_Miles" : 171.28646239400001,
+          "Acres" : 109611.03126600001,
+          "Name" : "CapeFear",
+          "ORIG_FID" : 12
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 13,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -77.0619,
+            35.5433
+          ]
+        },
+        "properties" : {
+          "FID" : 13,
+          "FID_1" : 14,
+          "Basin" : "Beaufort",
+          "Sq_Miles" : 6147.5707159200001,
+          "Acres" : 3934445.25819,
+          "Name" : "Tar Pamlico",
+          "ORIG_FID" : 13
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 14,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -81.8272,
+            36.2392
+          ]
+        },
+        "properties" : {
+          "FID" : 14,
+          "FID_1" : 15,
+          "Basin" : "Watauga",
+          "Sq_Miles" : 204.77446828199999,
+          "Acres" : 131055.659701,
+          "Name" : "Watauga",
+          "ORIG_FID" : 14
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 15,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -78.8236,
+            34.8356
+          ]
+        },
+        "properties" : {
+          "FID" : 15,
+          "FID_1" : 16,
+          "Basin" : "Onslow",
+          "Sq_Miles" : 1381.5199395499999,
+          "Acres" : 884172.76131099998,
+          "Name" : "White Oak",
+          "ORIG_FID" : 15
+        }
+      },
+      {
+        "type" : "Feature",
+        "id" : 16,
+        "geometry" : {
+          "type" : "Point",
+          "coordinates" : [
+            -80.4439,
+            36.1317
+          ]
+        },
+        "properties" : {
+          "FID" : 16,
+          "FID_1" : 17,
+          "Basin" : "Forsyth",
+          "Sq_Miles" : 7220.8593455700002,
+          "Acres" : 4621349.98116,
+          "Name" : "Yadkin Pee Dee",
+          "ORIG_FID" : 16
+        }
+      }
+    ]
+  };
+ 
